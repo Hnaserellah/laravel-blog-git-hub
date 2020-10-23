56af9c32ff08d1e7278ea45d82edd564227b854f
Hnaserellah <hazimnaser8991@gmail.com>
2020-10-23 12:24:47 +0100

    bien ajouté

---
+++ app/Models/Post.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Post extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    public function users() {
+        return $this->belongsTo('\App\Models\User');
+    }
+
+    public function categories() {
+        return $this->belongsTo('\App\Models\categories');
+    }
+
+    public function comments() {
+        return $this->hasMany('App\Model\Comment');
+    }
+
+}
